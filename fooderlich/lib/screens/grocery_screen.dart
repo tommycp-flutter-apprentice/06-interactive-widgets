4cdc53d8b224229544638ee44094344142cd4cf7
tommycp96 <tommycp8@gmail.com>
2022-02-02 16:09:38 +0700

    6.7 Managing the grocery items

--- fooderlich/lib/screens/grocery_screen.dart
+++ fooderlich/lib/screens/grocery_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../models/models.dart';
+
 import 'package:fooderlich/screens/empty_grocery_screen.dart';
 import './grocery_screen.dart';
 
@@ -7,8 +10,39 @@ class GroceryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO 4: Add a scaffold widget
-    return EmptyGroceryScreen();
+    // The main layout structure for GroceryScreen is a scaffold.
+    return Scaffold(
+      // Adds a floating action button with a + icon.
+      // Tapping the button presents the screen to create or add an item.
+      // You’ll build this screen later.
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          // TODO 11: Present GroceryItemScreen
+        },
+      ),
+      // Builds the rest of the Grocery screen’s subtree. That’s coming up next!
+      body: buildGroceryScreen(),
+    );
   }
-  // TODO: Add buildGroceryScreen
+
+  Widget buildGroceryScreen() {
+    // You wrap your widgets inside a Consumer,
+    // which listens for GroceryManager state changes.
+    return Consumer<GroceryManager>(
+      // Consumer rebuilds the widgets below itself when
+      // the grocery manager items changes.
+      builder: (context, manager, child) {
+        // If there are grocery items in the list, show the GroceryListScreen.
+        // You will create this screen soon.
+        if (manager.groceryItems.isNotEmpty) {
+          // TODO 25: Add GroceryListScreen
+          return Container();
+        } else {
+          // If there are no grocery items, show the EmptyGroceryScreen.
+          return const EmptyGroceryScreen();
+        }
+      },
+    );
+  }
 }
