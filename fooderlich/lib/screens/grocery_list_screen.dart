ed6647c49ed508a28f357f904e0bd009df8c5670
tommycp96 <tommycp8@gmail.com>
2022-02-03 09:33:46 +0700

    6.15 Adding gestures

--- fooderlich/lib/screens/grocery_list_screen.dart
+++ fooderlich/lib/screens/grocery_list_screen.dart
@@ -22,19 +22,43 @@ class GroceryListScreen extends StatelessWidget {
         itemBuilder: (context, index) {
           final item = groceryItems[index];
           // TODO 28: Wrap in a Dismissable
-          // TODO 27: Wrap in an InkWell
-          // For each item in the list,
-          // get the current item and construct a GroceryTile.
-          return GroceryTile(
-            key: Key(item.id),
-            item: item,
-            // Return onComplete when the user taps the checkbox.
-            onComplete: (change) {
-              // Check if there is a change and update
-              // the item’s isComplete status.
-              if (change != null) {
-                manager.completeItem(index, change);
-              }
+          // You wrap GroceryTile inside an InkWell.
+          return InkWell(
+            // For each item in the list,
+            // get the current item and construct a GroceryTile.
+            child: GroceryTile(
+                key: Key(item.id),
+                item: item,
+                // Return onComplete when the user taps the checkbox.
+                onComplete: (change) {
+                  // Check if there is a change and update
+                  // the item’s isComplete status.
+                  if (change != null) {
+                    manager.completeItem(index, change);
+                  }
+                }),
+            // When the gesture recognizes a tap, it presents GroceryItemScreen,
+            // letting the user edit the current item.
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => GroceryItemScreen(
+                    originalItem: item,
+                    // GroceryItemScreen calls onUpdate when
+                    // the user updates an item.
+                    onUpdate: (item) {
+                      // GroceryManager updates the item at the particular index
+                      manager.updateItem(item, index);
+                      // Dismisses GroceryItemScreen.
+                      Navigator.pop(context);
+                    },
+                    // onCreate will not be called since
+                    // you are updating an existing item.
+                    onCreate: (item) {},
+                  ),
+                ),
+              );
             },
           );
         },
