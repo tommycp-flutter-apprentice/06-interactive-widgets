b1d3e36dff94af82a5c322a052b5bd2dc8e090be
tommycp96 <tommycp8@gmail.com>
2022-02-03 09:04:50 +0700

    6.14 Creating GroceryListScreen

---
+++ fooderlich/lib/screens/grocery_list_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import '../components/grocery_tile.dart';
+import '../models/models.dart';
+import 'grocery_item_screen.dart';
+
+class GroceryListScreen extends StatelessWidget {
+  final GroceryManager manager;
+
+  const GroceryListScreen({Key? key, required this.manager}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Get the list of grocery items from the manager.
+    final groceryItems = manager.groceryItems;
+    // Apply padding of 16 pixels all around this screen.
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      // Add ListView.
+      child: ListView.separated(
+        // Set the number of items in the list.
+        itemCount: groceryItems.length,
+        itemBuilder: (context, index) {
+          final item = groceryItems[index];
+          // TODO 28: Wrap in a Dismissable
+          // TODO 27: Wrap in an InkWell
+          // For each item in the list,
+          // get the current item and construct a GroceryTile.
+          return GroceryTile(
+            key: Key(item.id),
+            item: item,
+            // Return onComplete when the user taps the checkbox.
+            onComplete: (change) {
+              // Check if there is a change and update
+              // the item’s isComplete status.
+              if (change != null) {
+                manager.completeItem(index, change);
+              }
+            },
+          );
+        },
+        // Space each grocery item 16 pixels apart.
+        separatorBuilder: (context, index) {
+          return const SizedBox(height: 16.0);
+        },
+      ),
+    );
+  }
+}
--- fooderlich/lib/screens/grocery_screen.dart
+++ fooderlich/lib/screens/grocery_screen.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import '../models/models.dart';
 import 'empty_grocery_screen.dart';
 import 'grocery_item_screen.dart';
+import 'grocery_list_screen.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
@@ -64,7 +65,7 @@ class GroceryScreen extends StatelessWidget {
         // You will create this screen soon.
         if (manager.groceryItems.isNotEmpty) {
           // TODO 25: Add GroceryListScreen
-          return Container();
+          return GroceryListScreen(manager: manager);
         } else {
           // If there are no grocery items, show the EmptyGroceryScreen.
           return const EmptyGroceryScreen();
